74dc1b61d2239f05d1545268800289416395c0b4
Tribrit <131851806+Tribrit@users.noreply.github.com>
2025-05-08 23:57:02 +0300

    Update week1.py

--- week1.py
+++ week1.py
@@ -1,1 +1,14 @@
+from pet import Pet
+
+# Createing a new pet
+my_pet = Pet("Max")
+
+# Interacting  with the pet
+my_pet.eat()
+my_pet.play()
+my_pet.sleep()
+my_pet.train("roll over")
+my_pet.train("play dead")
+my_pet.show_tricks()
+my_pet.get_status()
 
